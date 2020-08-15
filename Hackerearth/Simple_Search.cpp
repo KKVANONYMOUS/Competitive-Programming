a1e6334909f8ffde7c6707289618df1cd1e7a1a6
Kunal Kumar Verma <ninja.kunal01@gmail.com>
2020-08-15 14:55:40 +0530

    code++

--- Hackerearth/Simple_Search.cpp
+++ Hackerearth/Simple_Search.cpp
@@ -3,27 +3,30 @@ using namespace std;
 
 #define ll          long long
 #define fl(i,a,n)   for(int i=a;i<n;i++)
-#define all(c)      c.begin(), c.end()
 #define vi          vector<int>
 #define vll         vector<ll>
-#define pii         pair<int,int>
 #define pb          push_back
-#define mp          make_pair
-#define f           first
-#define s           second
 #define mod         1000000007
 
 void kkvanonymous(){
 ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
 }
 
 int main(){
     kkvanonymous();
-    
+    int n,x,k;
+    vi a;
+    cin>>n;
+    fl(i,0,n){
+        cin>>x;
+        a.pb(x);
+    }
+    cin>>k;
+    fl(i,0,n){
+        if (a[i]==k){
+            cout<<i;
+            break;
+        }
+    }
     return 0;
 }
