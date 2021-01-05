73c8b1464ebc619c4e31e9b9ebb1ea92b2979ceb
kkvanonymous <ninja.kunal01@gmail.com>
2021-01-05 23:07:29 +0530

    Create NoOfWaysToReachNthStep.cpp

---
+++ Algorithms/Dynamic Programming/NoOfWaysToReachNthStep.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector <int> dp(10005,-1);
+
+int findNoOfWays(int n){
+    if (n==0 || n==1){
+        dp[0]=dp[1]=1;
+        return 1;
+    }
+    else if (n==2){
+        dp[2]=2;
+        return 2;
+    }
+    if (dp[n]!=-1) return dp[n];
+
+    dp[n]=findNoOfWays(n-1)+findNoOfWays(n-2)+findNoOfWays(n-3);
+    return dp[n];
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<findNoOfWays(n);
+}
