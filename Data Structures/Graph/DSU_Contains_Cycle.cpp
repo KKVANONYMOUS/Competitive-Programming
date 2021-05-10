df2d0e8462a7a1387948db6636aeeaea8bac7223
kkvanonymous <ninja.kunal01@gmail.com>
2021-05-10 12:48:18 +0530

    Optimizations-Path Compression and Union by rank

--- Data Structures/Graph/DSU_Contains_Cycle.cpp
+++ Data Structures/Graph/DSU_Contains_Cycle.cpp
@@ -25,6 +25,15 @@ class Graph{
                return findSet(parent[i],parent);
         }
 
+        //Find operation - Path Compression
+        //Time Complexity:O(log(N))
+        int findSetOptimised(int i,vector <int> &parent){
+               //base case
+               if(parent[i]==-1) return i;
+               //path compression optimization
+               return parent[i]=findSetOptimised(parent[i],parent);
+        }
+
         //Union operation
         //Time Complexity:O(N)
         void union_set(int a,int b,vector <int> &parent){
@@ -36,20 +45,39 @@ class Graph{
             }
         }
 
+        //Union operation - Union by rank
+        //Time Complexity:O(log(N))
+        void union_setOptimised(int a,int b,vector <int> &parent,vector <int> &rank){
+            int s1=findSetOptimised(a,parent);
+            int s2=findSetOptimised(b,parent);
+
+            if(s1!=s2){
+                if(rank[s1]<rank[s2]){
+                    parent[s1]=s2;
+                    rank[s2]+=rank[s1];
+                }
+                else{
+                    parent[s2]=s1;
+                    rank[s1]+=rank[s2];
+                }
+            }
+        }
+
         bool contains_cycle(){
-            //parent vector 
+            //parent and rank vector 
             vector <int> parent(vertices,-1);
+            vector <int> rank(vertices,1);
 
             //iterate over edge list
             for(auto x:edge){
                 int a=x.first;
                 int b=x.second;
 
-                int s1=findSet(a,parent);
-                int s2=findSet(b,parent);
+                int s1=findSetOptimised(a,parent);
+                int s2=findSetOptimised(b,parent);
 
                 //if parent same then cycle is found else we do union of the two sets
-                if(s1!=s2) union_set(a,b,parent);
+                if(s1!=s2) union_setOptimised(a,b,parent,rank);
                 else{
                     return true;
                 }
