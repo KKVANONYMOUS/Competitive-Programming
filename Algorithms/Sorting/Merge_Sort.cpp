cc7756dedc3ee55fe09064569eb1b3da283fdecc
kkvanonymous <ninja.kunal01@gmail.com>
2021-06-27 15:59:35 +0530

    Update Merge_Sort.cpp

--- Algorithms/Sorting/Merge_Sort.cpp
+++ Algorithms/Sorting/Merge_Sort.cpp
@@ -8,47 +8,33 @@ void printArr(vector <int> a){
     cout<<"\n";
 }
 
-void merge(vector <int> &a,int l,int m, int r){
-    int n1=m-l+1;
-    int n2=r-m;
-
-    vector <int> L(n1); // make temp vector L
-    vector <int> R(n2); // make temp vector R
-
-    for(int i=0;i<n1;i++)  L[i]=a[l+i]; // copy data to L
-    for(int j=0;j<n2;j++)  R[j]=a[m+1+j]; // copy data to R
-
-    int i=0; // initial index of first subvector
-    int j=0; // initial index of second subvector
-    int k=l; // initial index of merged subvector
-
-    while(i<n1 && j<n2){
-        if (L[i]<=R[j]){
-            a[k]=L[i];
-            i++;
+void merge(vector <int> &a,int l,int mid,int r){
+    int i=l,j=mid+1;
+    vector <int> temp(a.size(),0); //temp vector to store the merged arrays
+    int k=l;
+    while(i<=mid and j<=r){
+        if(a[i]<a[j]){
+            temp[k++]=a[i++];
         }
         else{
-            a[k]=R[j];
-            j++;
+            temp[k++]=a[j++];
         }
-        k++;
     }
 
-    // include all elements left in L
-    while(i < n1){
-        a[k]=L[i];
-        i++;
-        k++;
+    // include all elements left in LHS
+    while(i<=mid){
+        temp[k++]=a[i++];
     }
 
-    // include all elements left in R
-    while(j < n2){
-        a[k]=R[j];
-        j++;
-        k++;
+    // include all elements left in RHS
+    while(j<=r){
+        temp[k++]=a[j++];
+    }
+
+    for(int i=l;i<=r;i++){
+        a[i]=temp[i];
     }
 }
-
 void mergeSort(vector <int> &a,int l,int r){
     if (r>l){
         int m=(l+r)/2;
@@ -59,10 +45,38 @@ void mergeSort(vector <int> &a,int l,int r){
    
 }
 
+//Two Way Merge Sort
+void mergeSortIterative(vector <int> &a,int n){
+    int p;
+    for(p=2;p<=n;p*=2){ //no of passes == log(n)
+        int i;
+        for(i=0;i+p-1<n;i+=p){
+            int l=i;
+            int r=i+p-1;
+            int mid=(l+r)/2;
+            merge(a,l,mid,r);
+        }
+
+        // for some leftover elements eg.{2, 5, 8, 12, 3, 6, 7, 10, 11, 10, 2}
+        if(n-i>p/2)
+        {
+            int l=i;
+            int r=i+p-1;
+            int mid=(l+r)/2;
+            merge(a,l,mid,n-1);
+        }
+    }
+
+    //for odd no. of elements 
+    if(p/2<n){
+        merge(a,0,p/2-1,n-1);
+    }
+}
 int main(){
-    vector <int> a = { 12, 11, 13, 5, 6, 7 };
+    vector <int> a = { 12, 11, 13, 5, 6, 7, 6 };
     printArr(a); 
-    mergeSort(a,0,a.size()-1);
+    //mergeSort(a,0,a.size()-1);
+    mergeSortIterative(a,a.size());
     printArr(a);
     return 0;
 }
